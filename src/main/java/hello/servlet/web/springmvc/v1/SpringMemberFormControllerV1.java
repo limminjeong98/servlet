91af152a36805d5537a95cc4094392a2bcdc5558
HKMC\E183999 <limminjeong98@naver.com>
2021-09-27 13:32:12 +0900

    Spring MVC - Controller V1 comment added

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+// @Controller 대신에 @Component와 @RequestMapping을 같이 써도 된다.
+// RequestMappingHandlerMapping이 스프링 빈 중에서
+//  @RequestMapping 또는 @Controller가 클래스 레벨에 붙어있는 경우에 매핑 정보로 인식한다.
 @Controller
 public class SpringMemberFormControllerV1 {
 
